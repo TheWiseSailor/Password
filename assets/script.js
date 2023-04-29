6b95f84d8f0c07edd53d18de12caa968c9df181d
TheWiseSailor <68026214+TheWiseSailor@users.noreply.github.com>
2023-04-29 11:29:24 -0400

    v1.05 forgot  to actually change them into arrays

--- assets/script.js
+++ assets/script.js
@@ -3,12 +3,30 @@
 //I will start by defining the character sets
 //(rename it to lowersetChars because I
 //was too lazy to write out the whole thing)
-lowercaseChars = "abcdefghijklmnopqrstuvwxyz";
-uppercaseChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-numberChars = "0123456789";
-specialChars = "!\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~";
+var lowercaseChars = "abcdefghijklmnopqrstuvwxyz";
+var uppercaseChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var numberChars = "0123456789";
+var specialChars = "!\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~";
 //Converted the character sets into arrays
-const lowercaseChars = lowercaseChars.split("");
-const uppercaseChars = uppercaseChars.split("");
-const numberChars = numberChars.split("");
-const specialChars = specialChars.split("");
+const lowercaseArray = lowercaseChars.split("");
+const uppercaseArray = uppercaseChars.split("");
+const numberArray = numberChars.split("");
+const specialArray = specialChars.split("");
+// Initializing an array for storage of
+//characters for my password
+let possibleCharacters = [];
+// uncommenting the following lines below when needed
+//var password = generatePassword();
+//var generateBtn = document.querySelector("#generate");
+//var passwordText = document.querySelector("#password");
+
+//making a function to ask the user for a password length
+const getPasswordLength = () => {
+  let numberOfCharacters = ask(
+    "How many characters pwould you like in your password? Choose between 8-32 chracters."
+  );
+  return numberOfCharacters;
+};
+
+//A function to validate user input for password length
+const validatePasswordLength = Length;
