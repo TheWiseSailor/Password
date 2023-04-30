98f460f0a91177f2fbd69dafa81498f15dc85ee1
TheWiseSailor <68026214+TheWiseSailor@users.noreply.github.com>
2023-04-29 20:42:20 -0400

    v1.13, had the guts to find  the issues. LIST OF ISSUES (LINE 29, 40-43, 50-52, 60-63, 72, and 74) fixed all the bugs, and most of the grammer mistakes. GOOGLE DOC LINK OF DEBUG:https://docs.google.com/document/d/1Hs9-7pncT1sdVp2y9EN4RR1h8xvbagCfr4tBE0bt3oA/edit?usp=sharing

--- script.js
+++ script.js
@@ -1,66 +1,38 @@
-// Assignment code here
-//making a custom alert
 alert("Please Click Generate Password to get started");
-//I will start by defining the character sets
-//(rename it to lowersetChars because I
-//was too lazy to write out the whole thing)
 var lowercaseChars = "abcdefghijklmnopqrstuvwxyz";
 var uppercaseChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 var numberChars = "0123456789";
 var specialChars = "!\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~";
-//Converted the character sets into arrays
 const lowercaseArray = lowercaseChars.split("");
 const uppercaseArray = uppercaseChars.split("");
 const numberArray = numberChars.split("");
 const specialArray = specialChars.split("");
-// Initializing an array for storage of
-//characters for my password
 let possibleCharacters = [];
-// uncommenting the following lines below when needed
-//var password = generatePassword();
-var generateBtn = document.querySelector("#generate");
-var passwordText = document.querySelector("#password");
-
-//making a function to ask the user for a password length
+const generateBtn = document.querySelector("#generate");
+const passwordText = document.querySelector("#password");
 const getPasswordLength = () => {
   let numberOfCharacters = prompt(
-    "How many characters pwould you like in your password? Choose between 8-32 chracters."
+    "How many characters do you want in your password? Choose between 8-32 characters."
   );
   return numberOfCharacters;
 };
-
-//A function to validate user input for password length
 const validatePasswordLength = (length) => {
   while (length < 8 || length > 32 || isNaN(length)) {
-    return length;
+    length = prompt("Please enter a valid number between 8 and 32.");
   }
+  return length;
 };
-// this will make the functions to ask
-//the user for the different types of
-// characters to which they can include
-//within the password
-const getIncludeLowercase = () =>
-  confirm("Would you like to include lowercase characters?");
-const getIncludeUppercase = () =>
-  confirm("Would you like to include uppercase characters?");
-const getIncludeNumbers = () => confirm("Would you like to include numbers?");
-const getIncludeSpecial = () =>
-  confirm("Would you like to include lany special characters?");
-
-//this function will be able to verify that there is at
-//least one character type that it needs to include
-//within the password
+const getIncludeLowercase = () => confirm("Do you want lowercase characters?");
+const getIncludeUppercase = () => confirm("Do you want uppercase characters?");
+const getIncludeNumbers = () => confirm("Do you want to use numbers?");
+const getIncludeSpecial = () => confirm("Do you want special characters?");
 const validateCharacterTypes = (lowercase, uppercase, numbers, special) => {
-  alert("Please select at least one character type.");
-  {
+  if (!lowercase && !uppercase && !numbers && !special) {
+    alert("Please select at least one character type.");
     return false;
   }
   return true;
 };
-
-//this will be the section to where I make functions to add the characters selected
-// to possibleCharacters array
-
 const addLowercaseCharacters = () =>
   (possibleCharacters = possibleCharacters.concat(lowercaseArray));
 const addUppercaseCharacters = () =>
@@ -69,60 +41,54 @@ const addNumberCharacters = () =>
   (possibleCharacters = possibleCharacters.concat(numberArray));
 const addSpecialCharacters = () =>
   (possibleCharacters = possibleCharacters.concat(specialArray));
-
-//adding a function to make a random password based on the user's input
-
 const generateRandomPassword = (length) => {
   let password = "";
   for (let i = 0; i < length; i++) {
     const randomIndex = Math.floor(Math.random() * possibleCharacters.length);
+    password += possibleCharacters[randomIndex];
   }
+  return password;
 };
-
-//creating a function to where it generates a password
 const generatePassword = () => {
   let numberOfCharacters = getPasswordLength();
   numberOfCharacters = validatePasswordLength(numberOfCharacters);
-  // I FORGOT TO ADD THIS
+
   //to where it asks the user for which types of chracters to include, in the password
   const hasLowercase = getIncludeLowercase();
   const hasUppercase = getIncludeUppercase();
   const hasNumbers = getIncludeNumbers();
   const hasSpecial = getIncludeSpecial();
-  //the console asks the user what type of characters to include in the password
-  const ValidCharacterTypes = validateCharacterTypes(
+  const isValidCharacterTypes = validateCharacterTypes(
     hasLowercase,
     hasUppercase,
     hasNumbers,
     hasSpecial
   );
-  if (!ValidCharacterTypes) {
+  if (!isValidCharacterTypes) {
     return;
   }
-
-  //I added selected characters to possibleCharacters array
   if (hasLowercase) {
     addLowercaseCharacters();
   }
   if (hasUppercase) {
     addUppercaseCharacters();
   }
+  if (hasNumbers) {
+    addNumberCharacters();
+  }
   if (hasSpecial) {
     addSpecialCharacters();
   }
-  //This below will generate a password by picking
-  //random characters from the possibleCharacters array
   let password = generateRandomPassword(numberOfCharacters);
   possibleCharacters = [];
+
   return password;
-  // this makes it to where it clears it for
-  // security reasons and etc
 };
-//write the password to the DOM
 const writePassword = () => {
+  const password = generatePassword();
   if (password) {
-    password.value = password;
+    passwordText.value = password;
   }
 };
 
-generateBtn.addEventLister("click", writePassword);
+generateBtn.addEventListener("click", writePassword);
